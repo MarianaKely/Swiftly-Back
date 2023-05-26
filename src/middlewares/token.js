db97763dcccde2831e518ec92b7c648c02af7bcd
MarianaKely <marianakely.r@gmail.com>
2023-05-26 00:17:07 -0300

    middleware implemented

---
+++ src/middlewares/token.js
@@ -0,0 +1,17 @@
+
+
+export default function authoToken(req, res, next) {
+
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) 
+    return res.sendStatus(401);
+    console.log('not authorized');
+
+    res.locals.token = token;
+
+    next();
+    console.log('ok');
+
+  }
