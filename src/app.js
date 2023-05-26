780620d5cfb1b5218884013204c46924328e5f27
MarianaKely <marianakely.r@gmail.com>
2023-05-26 00:22:19 -0300

    app.js page with routes implemented

--- src/app.js
+++ src/app.js
@@ -2,12 +2,23 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import registerRouter from "./routers/registerRouter.js";
+import friendsRouter from "./routers/friendsRouter.js";
+import postRouter from "./routers/postRouter.js";
+import userRouter from "./routers/userRouter.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(registerRouter);
+app.use(friendsRouter);
+app.use(postRouter);
+app.use(userRouter);
+
+
 
 
 const PORT = process.env.PORT || 5000;
